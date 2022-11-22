6cb016d33e52a92c7040d1289c864c68ed0dd185
huangwei18351 <496928366@qq.com>
2022-11-22 14:38:20 +0800

    test free

--- demo.js
+++ demo.js
@@ -22,10 +22,7 @@ while(!id("city_arrow").exists()){
 	back();
 	sleep(sleepTime);
 }
-// if(text("国内(含港澳台)").findOnce()){
-// 	back();
-// 	sleep(sleepTime);
-// }
+
 //判断city 是不是上海
 let b ;
 var isShanghai = text("上海").findOnce();
@@ -60,7 +57,7 @@ console.log("in shanghai");
 //进入免费试
 var freeTry ;
 id("main_listview").findOne().children().forEach(child => {
-	freeTry = className("android.widget.ImageButton").desc("免费试瓷片位（10.32.0以上Picasso）-活动后恢复").findOne();
+	freeTry = className("android.widget.ImageButton").descContains("免费试").findOne();
 });
 freeTry.click();
 toast("进入免费试");
@@ -97,52 +94,29 @@ let slideHight = 348 * 5;
 let swipeBtY = 2046;
 let swipeBtX = device.width - 200;
 let swipeToY = swipeBtY - slideHight;
-
-
-
-var categories = className("android.widget.TextView").text("全部分类").findOne().parent();
-b = categories.bounds();
-click(b.centerX(), b.centerY());
-sleep(sleepTime);
-
-var food = className("android.widget.TextView").text("美食").findOne().parent();
-b = food.bounds();
-// console.log(device.width - 100);
-click(device.width - 100, b.centerY());
-toast("点击美食");
-sleep(sleepTime);
-
-//筛选
-var selectBtn = className("android.widget.TextView").text("筛选").findOne().parent();
-b = selectBtn.bounds();
-click(b.centerX(), b.centerY());
-sleep(sleepTime);
-
-var notApply = className("android.widget.TextView").text("只看未抢").findOne().parent();
-b = notApply.bounds();
-click(device.width - 100, b.centerY());
-toast("点击只看未抢");
-sleep(sleepTime);
-
-// do{
-// //点击免费抽
+do{
+//点击免费抽
 
 	var list = className("android.support.v7.widget.RecyclerView").scrollable(true).depth(13).findOne().children();
+	console.log(list.size());
 	for(var i = 0; i < list.size() - 1; i++){
 		var child = list.get(i);
 		//找到那一栏，说明是上面的，需要跳过
 		if(child.findOne(text("美食")) != null){
 			continue;
 		}
+		console.log("apply for the " + (num++) + " free of charge meal.");
 		var target = child.findOne(text("免费抽"));
-		let bound = target.bounds();
-		// console.log(bound.centerX(), bound.centerY());
-		click(device.width - 200, bound.centerY());
-		sleep(sleepTime);
-		
-		applyFood();
+		if(target != null){
+			let bound = target.bounds();
+			click(device.width - 200, bound.centerY());
+			sleep(sleepTime);
+			applyFood();
+		}
 	}
-// }while(className("android.widget.TextView").text("当前无更多活动，请耐心等待~").findOnce() == null);
+	swipe(swipeBtX, swipeBtY, swipeBtX, swipeToY, 3*sleepTime);
+	sleep(sleepTime);
+}while(className("android.widget.TextView").text("当前无更多活动，请耐心等待~").findOnce() == null);
 
 function backToFree(){
 	do {
@@ -155,6 +129,7 @@ function applyFood(){
 	var apply = text("我要报名").findOnce();
 	if(apply == null){
 		backToFree();
+		return;
 	}
 	let bound = apply.bounds();
 	click(device.width - 200, bound.centerY());
@@ -162,39 +137,43 @@ function applyFood(){
 	var confirmApply = text("确认报名").findOnce();
 	if(confirmApply == null){
 		backToFree();
+		return ;
 	}
 
-	var chooseStore = text("请选择分店").findOnce();
+	var chooseStore = textContains("请选择").findOnce();
 	if(chooseStore != null){
 		bound = chooseStore.bounds();
-		click(bound.centerX(), bound.centerY());
+		click(device.width - 200, bound.centerY());
+		// console.log("点击请选择店");
 		sleep(sleepTime);
 
-		var store = textContain("中奖名额").findOne();
-		if(store != null){
-			bound = chooseStore.bounds();
-			click(bound.centerX(), bound.centerY());
-			sleep(sleepTime);
-		}
-	}
-
-	chooseStore = text("请选择分店").findOnce();
-	if(chooseStore != null){
-		bound = chooseStore.bounds();
-		click(bound.centerX(), bound.centerY());
-		sleep(sleepTime);
-
-		var store = textContain("中奖名额").findOne();
+		var store = className("android.widget.TextView").textContains("距您").findOnce();
 		if(store != null){
 			bound = store.bounds();
-			click(bound.centerX(), bound.centerY());
+			click(1000, bound.centerY());
 			sleep(sleepTime);
 		}
 	}
-	var unJoin = text("未参与").findOnce();
+
+	chooseStore = textContains("请选择").findOnce();
+	if(chooseStore != null){
+		bound = chooseStore.bounds();
+		click(device.width - 200, bound.centerY());
+		// console.log("点击请选择分店");
+		sleep(sleepTime);
+
+		var store = className("android.widget.TextView").textContains("距您").findOnce();
+		if(store != null){
+			bound = store.bounds();
+			click(1000, bound.centerY());
+			sleep(sleepTime);
+		}
+	}
+	var unJoin = text("未参加").findOnce();
 	if(unJoin != null){
 		bound = unJoin.bounds();
 		click(1000, bound.centerY());
+		// console.log("点击未参加");
 		sleep(sleepTime);
 	}
 
