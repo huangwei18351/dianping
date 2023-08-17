8b0db5cb8e8ad32c49af8eac34e84bccb54272c9
huangwei18351 <496928366@qq.com>
2023-08-17 21:10:51 +0800

    dianping

--- demo.js
+++ demo.js
@@ -6,226 +6,235 @@ if (auto.service == null) {
 
 var storage = storages.create("select");
 var selectArr = storage.get("selected");
-toast(selectArr[0]);
-// auto.waitFor();
-// console.show(); //开启日志（悬浮窗权限）
-// console.setPosition(0, 100);
+if(selectArr.length == 0){
+    selectArr = new Array(2);
+    selectArr[0] = "美食";
+    selectArr[1] = "丽人";
+}
 
-// threads.start(function(){  //开启子线程
-// 	//监听音量键-，关闭所有脚本
-// 	events.observeKey();
-// 	events.onKeyDown("volume_down",function(event){//音量+改为volume_up
-// 	  engines.stopAllAndToast();    
-// 	 });
+auto.waitFor();
+console.show(); //开启日志（悬浮窗权限）
+console.setPosition(0, 100);
+
+threads.start(function(){  //开启子线程
+	//监听音量键-，关闭所有脚本
+	events.observeKey();
+	events.onKeyDown("volume_down",function(event){//音量+改为volume_up
+	  engines.stopAllAndToast();    
+	 });
  
-//  });
+ });
  
 
-// let sleepTime = 1500;
-// sleep(sleepTime);
-// app.launchApp("大众点评");
-// // console.setSize(sleepTime,2000)
-// sleep(sleepTime);
+let sleepTime = 1500;
+sleep(sleepTime);
+app.launchApp("大众点评");
+// console.setSize(sleepTime,2000)
+sleep(sleepTime);
 
 
-// toast("open dianping");
-// sleep(sleepTime);
+toast("open dianping");
+sleep(sleepTime);
 
-// backToIndex();
+backToIndex();
 
-// let num = 1;
-// //翻页，每次翻5个
-// let slideHight = 348 * 5;
-// let swipeBtY = 2046;
-// let swipeBtX = device.width - 200;
-// let swipeToY = swipeBtY - slideHight;
-// let b ;
+let num = 1;
+//翻页，每次翻5个
+let slideHight = 348 * 5;
+let swipeBtY = 2046;
+let swipeBtX = device.width - 200;
+let swipeToY = swipeBtY - slideHight;
+let b ;
 
-// for(var cycle = 0; cycle < 2; cycle ++){
-// 	//go to free
-// 	enterIn();
-// 	clickCat();
-// 	doWhileForFree();
-// 	backToIndex();
-// }
+for(var i = 0; i < selectArr.length; i++){
+    var item = selectArr[i];
+    for(var cycle = 0; cycle < 2; cycle ++){
+        //go to free
+        enterIn();
+        clickCat(item);
+        doWhileForFree();
+        backToIndex();
+    }
+}
 
-// console.log("end");
 
-// function enterIn(){
-// 	//判断city 是不是上海
+console.log("end");
+
+function enterIn(){
+	//判断city 是不是上海
 	
-// 	var isShanghai = text("上海").findOnce();
-// 	if(isShanghai == null){
-// 		if(id("city_arrow").exists()){
-// 			console.log("click city_arrow");
-// 			b = id("city_arrow").findOne().bounds();
-// 			click(b.centerX(), b.centerY());
-// 			sleep(sleepTime);
-// 		}
-// 		var arr = className("android.widget.TextView").text("上海").find();
-// 		if(arr.empty()){
-// 			toast("not found");
-// 			sleep(sleepTime);
-// 		}else{
-// 			let i = 1;
-// 			arr.forEach(function(a){
-// 				let isClick = a.click();
-// 				if(isClick){
-// 					toast("上海" + i + "被点击了");
-// 				}
-// 				i++;
-// 				sleep(sleepTime);
-// 			});
-// 		}
-// 		console.log(b);
+	var isShanghai = text("上海").findOnce();
+	if(isShanghai == null){
+		if(id("city_arrow").exists()){
+			console.log("click city_arrow");
+			b = id("city_arrow").findOne().bounds();
+			click(b.centerX(), b.centerY());
+			sleep(sleepTime);
+		}
+		var arr = className("android.widget.TextView").text("上海").find();
+		if(arr.empty()){
+			toast("not found");
+			sleep(sleepTime);
+		}else{
+			let i = 1;
+			arr.forEach(function(a){
+				let isClick = a.click();
+				if(isClick){
+					toast("上海" + i + "被点击了");
+				}
+				i++;
+				sleep(sleepTime);
+			});
+		}
+		console.log(b);
 
-// 		// click(b.centerX(), b.centerY());
-// 		console.log("choose city shanghai");
-// 	}
-// 	console.log("in shanghai");
-// 	//进入免费试
-// 	var freeTry ;
-// 	id("main_listview").findOne().children().forEach(child => {
-// 		freeTry = className("android.widget.ImageButton").descContains("免费试").findOne();
-// 	});
-// 	freeTry.click();
-// 	toast("进入免费试");
-// 	sleep(sleepTime);
-// }
+		// click(b.centerX(), b.centerY());
+		console.log("choose city shanghai");
+	}
+	console.log("in shanghai");
+	//进入免费试
+	var freeTry ;
+	id("main_listview").findOne().children().forEach(child => {
+		freeTry = className("android.widget.ImageButton").descContains("免费试").findOne();
+	});
+	freeTry.click();
+	toast("进入免费试");
+	sleep(sleepTime);
+}
 
-// function clickCat(){
-// 	var categories = className("android.widget.TextView").text("全部分类").find();
-// 	for(var i = 0; i < categories.size(); i++){
-// 		var cat = categories.get(i);
-// 		b = cat.bounds();
-// 		if(b.centerY() < 1000 || b.centerY() > 1500){
-// 			continue;
-// 		}
-// 		click(b.centerX(), b.centerY());
-// 		sleep(sleepTime);
-// 	}
+function clickCat(item){
+	var categories = className("android.widget.TextView").text("全部分类").find();
+	for(var i = 0; i < categories.size(); i++){
+		var cat = categories.get(i);
+		b = cat.bounds();
+		if(b.centerY() < 1000 || b.centerY() > 1500){
+			continue;
+		}
+		click(b.centerX(), b.centerY());
+		sleep(sleepTime);
+	}
 
-// 	var food = className("android.widget.TextView").text("美食").findOne().parent();
-// 	b = food.bounds();
-// 	// console.log(device.width - 100);
-// 	click(device.width - 100, b.centerY());
-// 	toast("点击美食");
-// 	sleep(sleepTime);
+	var food = className("android.widget.TextView").text(item).findOne().parent();
+	b = food.bounds();
+	// console.log(device.width - 100);
+	click(device.width - 100, b.centerY());
+	toast("点击美食");
+	sleep(sleepTime);
 
-// 	//筛选
-// 	var selectBtn = className("android.widget.TextView").text("更多筛选").findOne();
-// 	b = selectBtn.bounds();
-// 	click(b.centerX(), b.centerY());
-// 	sleep(sleepTime);
+	//筛选
+	var selectBtn = className("android.widget.TextView").text("更多筛选").findOne();
+	b = selectBtn.bounds();
+	click(b.centerX(), b.centerY());
+	sleep(sleepTime);
 
-// 	var notApply = className("android.widget.TextView").text("只看未报名").findOne().parent();
-// 	b = notApply.bounds();
-// 	click(device.width - 100, b.centerY());
-// 	toast("点击只看未抢");
-// 	sleep(sleepTime);
-// }
+	var notApply = className("android.widget.TextView").text("只看未报名").findOne().parent();
+	b = notApply.bounds();
+	click(device.width - 100, b.centerY());
+	toast("点击只看未抢");
+	sleep(sleepTime);
+}
 
-// function doWhileForFree(){
-// 	do{
-// 		//点击免费抽
+function doWhileForFree(){
+	do{
+		//点击免费抽
 		
-// 			var freeFreeList = className("android.widget.TextView").text("免费抽").find();
-// 			for(var i = 0; i < freeFreeList.size() - 1; i++){
+			var freeFreeList = className("android.widget.TextView").text("免费抽").find();
+			for(var i = 0; i < freeFreeList.size() - 1; i++){
 				
-// 				var free = freeFreeList.get(i);
-// 				let bound = free.bounds();
-// 				if(bound.centerX() < 890 || bound.centerX() > 1000 || bound.centerY() <= 351){
-// 					continue ;
-// 				}
-// 				console.log(bound.centerX(), bound.centerY());
-// 				click(device.width - 200, bound.centerY());
-// 				sleep(sleepTime);
-// 				applyFood();
-// 			}
-// 			swipe(swipeBtX, swipeBtY, swipeBtX, swipeToY, 3*sleepTime);
-// 			sleep(sleepTime);
-// 	}while(className("android.widget.TextView").textContains("当前无更多活动").findOnce() == null);
-// }
+				var free = freeFreeList.get(i);
+				let bound = free.bounds();
+				if(bound.centerX() < 890 || bound.centerX() > 1000 || bound.centerY() <= 351){
+					continue ;
+				}
+				console.log(bound.centerX(), bound.centerY());
+				click(device.width - 200, bound.centerY());
+				sleep(sleepTime);
+				applyFood();
+			}
+			swipe(swipeBtX, swipeBtY, swipeBtX, swipeToY, 3*sleepTime);
+			sleep(sleepTime);
+	}while(className("android.widget.TextView").textContains("当前无更多活动").findOnce() == null);
+}
 
-// function backToIndex(){
-// 	//back to index page
-// 	while(!id("city_arrow").exists()){
-// 		back();
-// 		sleep(sleepTime);
-// 	}
-// }
+function backToIndex(){
+	//back to index page
+	while(!id("city_arrow").exists()){
+		back();
+		sleep(sleepTime);
+	}
+}
 
-// function backToFree(){
-// 	do {
-// 		back();
-// 		sleep(sleepTime);
-// 	}while(className("android.widget.TextView").text("免费抽").findOnce() == null);
-// }
+function backToFree(){
+	do {
+		back();
+		sleep(sleepTime);
+	}while(className("android.widget.TextView").text("免费抽").findOnce() == null);
+}
 
-// function applyFood(){
-// 	var apply = text("我要报名").findOnce();
-// 	if(apply == null){
-// 		backToFree();
-// 		return;
-// 	}
-// 	let bound = apply.bounds();
-// 	click(device.width - 200, bound.centerY());
-// 	sleep(sleepTime);
+function applyFood(){
+	var apply = text("我要报名").findOnce();
+	if(apply == null){
+		backToFree();
+		return;
+	}
+	let bound = apply.bounds();
+	click(device.width - 200, bound.centerY());
+	sleep(sleepTime);
 
-// 	var confirmApply = text("知道啦，继续报名").findOnce();
-// 	if(confirmApply != null){
-// 		bound = chooseStore.bounds();
-// 		click(bound.centerX(), bound.centerY());
-// 		sleep(sleepTime);
-// 	}
+	var confirmApply = text("知道啦，继续报名").findOnce();
+	if(confirmApply != null){
+		bound = chooseStore.bounds();
+		click(bound.centerX(), bound.centerY());
+		sleep(sleepTime);
+	}
 
-// 	var confirmApply = text("确认报名").findOnce();
-// 	if(confirmApply == null){
-// 		backToFree();
-// 		return ;
-// 	}
+	var confirmApply = text("确认报名").findOnce();
+	if(confirmApply == null){
+		backToFree();
+		return ;
+	}
 
-// 	var chooseStore = textContains("请选择").findOnce();
-// 	if(chooseStore != null){
-// 		bound = chooseStore.bounds();
-// 		click(device.width - 200, bound.centerY());
-// 		// console.log("点击请选择店");
-// 		sleep(sleepTime);
+	var chooseStore = textContains("请选择").findOnce();
+	if(chooseStore != null){
+		bound = chooseStore.bounds();
+		click(device.width - 200, bound.centerY());
+		// console.log("点击请选择店");
+		sleep(sleepTime);
 
-// 		var store = className("android.widget.TextView").textContains("距您").findOnce();
-// 		if(store != null){
-// 			bound = store.bounds();
-// 			click(1000, bound.centerY());
-// 			sleep(sleepTime);
-// 		}
-// 	}
+		var store = className("android.widget.TextView").textContains("距您").findOnce();
+		if(store != null){
+			bound = store.bounds();
+			click(1000, bound.centerY());
+			sleep(sleepTime);
+		}
+	}
 
-// 	chooseStore = textContains("请选择").findOnce();
-// 	if(chooseStore != null){
-// 		bound = chooseStore.bounds();
-// 		click(device.width - 200, bound.centerY());
-// 		// console.log("点击请选择分店");
-// 		sleep(sleepTime);
+	chooseStore = textContains("请选择").findOnce();
+	if(chooseStore != null){
+		bound = chooseStore.bounds();
+		click(device.width - 200, bound.centerY());
+		// console.log("点击请选择分店");
+		sleep(sleepTime);
 
-// 		var store = className("android.widget.TextView").textContains("距您").findOnce();
-// 		if(store != null){
-// 			bound = store.bounds();
-// 			click(1000, bound.centerY());
-// 			sleep(sleepTime);
-// 		}
-// 	}
-// 	var unJoin = text("未参加").findOnce();
-// 	if(unJoin != null){
-// 		bound = unJoin.bounds();
-// 		click(1000, bound.centerY());
-// 		// console.log("点击未参加");
-// 		sleep(sleepTime);
-// 	}
+		var store = className("android.widget.TextView").textContains("距您").findOnce();
+		if(store != null){
+			bound = store.bounds();
+			click(1000, bound.centerY());
+			sleep(sleepTime);
+		}
+	}
+	var unJoin = text("未参加").findOnce();
+	if(unJoin != null){
+		bound = unJoin.bounds();
+		click(1000, bound.centerY());
+		// console.log("点击未参加");
+		sleep(sleepTime);
+	}
 
-// 	bound = confirmApply.bounds();
-// 	click(1000, bound.centerY());
-// 	sleep(sleepTime);
+	bound = confirmApply.bounds();
+	click(1000, bound.centerY());
+	sleep(sleepTime);
 
-// 	text("报名成功").waitFor();
-// 	backToFree();
-// }
+	text("报名成功").waitFor();
+	backToFree();
+}
